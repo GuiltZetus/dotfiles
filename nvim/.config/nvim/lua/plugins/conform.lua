a39b58cd382cb74c5681f2d8635a0b1bc171a18c
Guilt <davikzetus@gmail.com>
2025-05-19 17:41:37 +0700

    add nvim config from home to mini

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -44,12 +44,12 @@ return {
     end,
   },
 
-  {
-    "zapling/mason-conform.nvim",
-    event = "VeryLazy",
-    dependencies = { "conform.nvim" },
-    config = function()
-      require "configs.mason-conform"
-    end,
-  },
+  -- {
+  --   "zapling/mason-conform.nvim",
+  --   event = "VeryLazy",
+  --   dependencies = { "conform.nvim" },
+  --   config = function()
+  --     require "configs.mason-conform"
+  --   end,
+  -- },
 }
