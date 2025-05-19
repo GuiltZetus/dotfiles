dd79225a2354bd6d09e1adfc565bd3ee53a057d0
GuiltZetus <davikzetus@gmail.com>
2025-05-19 18:37:39 +0700

    add some buffer split and jump keymap for nvim

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -6,5 +6,9 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "<leader>bv", vim.cmd.vsplit, { desc = "Vertical Split Buffer" })
+map("n", "<leader>bh", vim.cmd.vsplit, { desc = "Horizontal Split Buffer" })
+map("n", "<C-h>", "<CR>:wincmd h<CR>", { desc = "Jump to left buffer" })
+map("n", "<C-l>", "<CR>:wincmd l<CR>", { desc = "Jump to right buffer" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
